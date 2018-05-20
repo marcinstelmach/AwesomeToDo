9c9c791c432fe0d7da856938a41d631ff7a81d4a
Marcin Stelmach <marcinxd4@gmail.com>
2018-05-20 10:28:33 +0200

    Added custom exception and error code

---
+++ AwesomeToDo.Core/Exceptions/AwesomeToDoException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeToDo.Core.Exceptions
+{
+    public class AwesomeToDoException : Exception
+    {
+        public ErrorCode ErrorCode { get; }
+
+        public AwesomeToDoException(ErrorCode errorCode)
+            : this(errorCode, string.Empty)
+        {
+
+        }
+
+        public AwesomeToDoException(ErrorCode errorCode, string message)
+            : this(errorCode, message, null)
+        {
+
+        }
+
+        public AwesomeToDoException(ErrorCode errorCode, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ AwesomeToDo.Core/Exceptions/ErrorCode.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace AwesomeToDo.Core.Exceptions
+{
+    public class ErrorCode
+    {
+        public string Message { get; protected set; }
+        public string Code { get; set; }
+        public HttpStatusCode HttpStatusCode { get; }
+
+        public ErrorCode(string message, HttpStatusCode httpStatusCode = HttpStatusCode.BadRequest)
+        {
+            Message = message;
+            HttpStatusCode = httpStatusCode;
+        }
+
+        public static ErrorCode EmptyCommand => new ErrorCode(nameof(EmptyCommand));
+        public static ErrorCode InvalidCommand => new ErrorCode(nameof(InvalidCommand));
+    }
+}
