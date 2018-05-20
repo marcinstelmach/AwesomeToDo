dbd17632924fd5cab38db97a83232869fed5df94
Marcin Stelmach <marcinxd4@gmail.com>
2018-05-20 14:00:46 +0200

    Added EF Configuration

---
+++ AwesomeToDo.Domain/Data/Concrete/Configurations/CardConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AwesomeToDo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AwesomeToDo.Domain.Data.Concrete.Configurations
+{
+    internal class CardConfiguration : IEntityTypeConfiguration<Card>
+    {
+        public void Configure(EntityTypeBuilder<Card> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Title).HasMaxLength(30);
+
+            builder.HasMany(s => s.ToDos)
+                .WithOne(s => s.Card)
+                .HasForeignKey("CardId")
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
---
+++ AwesomeToDo.Domain/Data/Concrete/Configurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AwesomeToDo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AwesomeToDo.Domain.Data.Concrete.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Email).HasMaxLength(30);
+            builder.HasIndex(s => s.Email);
+            builder.Property(s => s.FirstName).HasMaxLength(30);
+            builder.Property(s => s.LastName).HasMaxLength(30);
+
+            builder.HasMany(s => s.Cards)
+                .WithOne(s => s.User)
+                .HasForeignKey("UserId")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
