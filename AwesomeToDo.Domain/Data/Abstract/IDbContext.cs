737867a386b6590a252f0940e1455bd826dc703e
Marcin Stelmach <marcinxd4@gmail.com>
2018-05-20 13:15:45 +0200

    Changed projects to last minor c# version and .Net Core 2.1

---
+++ AwesomeToDo.Domain/Data/Abstract/IDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AwesomeToDo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeToDo.Domain.Data.Abstract
+{
+    public interface IDbContext
+    {
+        DbSet<User> Users { get; set; } 
+        DbSet<Card> Cards { get; set; }
+        DbSet<ToDo> ToDos { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
