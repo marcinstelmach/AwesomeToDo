cdfe55d37481d3a265b8aa8c94bf2a2221b89ce0
Marcin Stelmach <marcinxd4@gmail.com>
2018-06-05 21:59:09 +0200

    Added Command dispatcher, model, handler

---
+++ AwesomeToDo.Infrastructure/Commands/Abstract/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AwesomeToDo.Infrastructure.Commands.Abstract
+{
+    public interface ICommandHandler<T> where T : ICommandModel
+    {
+        Task HandleAsync(T command);
+    }
+}
---
+++ AwesomeToDo.Infrastructure/Commands/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using AwesomeToDo.Core.Exceptions;
+using AwesomeToDo.Infrastructure.Commands.Abstract;
+using NLog;
+
+namespace AwesomeToDo.Infrastructure.Commands.Dispatchers
+{
+    internal class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IComponentContext componentContext;
+        private readonly ILogger logger;
+
+        public CommandDispatcher(IComponentContext componentContext, ILogger logger)
+        {
+            this.componentContext = componentContext;
+            this.logger = logger;
+        }
+
+        public async Task DispatchAsync<T>(T command) where T : ICommandModel
+        {
+            logger.Trace($"Command has been run: {typeof(T)}");
+
+            if (command == null)
+            {
+                throw new AwesomeToDoException(ErrorCode.EmptyCommand, typeof(T).Name);
+            }
+
+            var handler = componentContext.Resolve<ICommandHandler<T>>();
+            await handler.HandleAsync(command);
+
+        }
+    }
+}
