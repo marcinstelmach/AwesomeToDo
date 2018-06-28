94516d3efcf15cfbaf2a0ea87fabc57add966dc1
Marcin Stelmach <marcinxd4@gmail.com>
2018-06-28 21:47:00 +0200

    Added migration

---
+++ AwesomeToDo.Domain/Migrations/20180628194146_RemovedContentFromMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AwesomeToDo.Domain.Migrations
+{
+    public partial class RemovedContentFromMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "ToDos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "ToDos",
+                nullable: true);
+        }
+    }
+}
