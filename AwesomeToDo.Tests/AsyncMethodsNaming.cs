f44e6e4f93c7102fb79dd3e93493ea2bc5ae020b
marcinstelmach <marcinxd4@gmail.com>
2018-08-18 16:11:00 +0200

    Added test for checking valid Async method names

---
+++ AwesomeToDo.Tests/AsyncMethodsNaming.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using AwesomeToDo.Api.Controllers;
+using AwesomeToDo.Core.Exceptions;
+using AwesomeToDo.Domain.Entities;
+using AwesomeToDo.Infrastructure.Services.Abstract.Commands;
+using Castle.Core.Internal;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Internal;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AwesomeToDo.Tests
+{
+    public class AsyncMethodsNaming
+    {
+        private readonly ITestOutputHelper output;
+
+        public AsyncMethodsNaming(ITestOutputHelper output)
+        {
+            this.output = output;
+        }        
+
+        [Theory]
+        [InlineData(typeof(ApiController))]
+        [InlineData(typeof(AwesomeToDoException))]
+        [InlineData(typeof(Card))]
+        [InlineData(typeof(ICardCommandService))]
+        public void AsyncMethodsHasValidNamesIn_Api_Namespace(Type type)
+        {
+            // arrange
+            var types = Assembly.GetAssembly(type).GetTypes();
+
+            // act
+            var result = types.GetInvalidAsyncMethods();
+
+            //assert
+            if (!result.IsNullOrEmpty())
+            {
+                output.WriteLine(string.Join(',', result.Select(s => s.DisplayName())));
+            }
+            result.Should().BeEmpty();
+        }
+    }
+}
---
+++ AwesomeToDo.Tests/TestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AwesomeToDo.Tests
+{
+    public static class TestExtensions
+    {
+        public static IEnumerable<MethodInfo> GetInvalidAsyncMethods(this Type[] types)
+            => types.SelectMany(c => c.GetMethods())
+                .Where(m => m.ReturnType.IsAssignableFrom(typeof(Task<>)) && !m.Name.EndsWith("Async"));
+    }
+}
